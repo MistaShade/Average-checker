0c48cfe3b9291614418a588b0131515ebf88712a
Sam <114481646+MistaShade@users.noreply.github.com>
2023-09-04 15:17:10 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Limitation = 5;
+            float sum = 0;
+
+            for (int l = 0; l < Limitation; l++)
+            {
+                Console.Write("Enter Number(You can only type 5 numbers sowwy :3): ");
+                sum += float.Parse(Console.ReadLine());
+
+            }
+
+            float ave = sum / Limitation;
+            Console.WriteLine($"The average {ave}");
+            Console.ReadKey();
+        }
+    }
+}
